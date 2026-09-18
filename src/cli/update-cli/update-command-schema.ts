TomSchuman/openclaw

fix(update): reconcile a managed service pinned to another install
Keep the active packaged CLI as the update target and reconcile verified writable Gateway services through the existing install owner. Show both local paths and versions even when Gateway metadata or the status session scan is unavailable.

Preserve native backups and rollback, keep unsafe older installations stopped after state maintenance, and retain the lightweight inspection guard through Doctor repair.

Fixes #150744. Thanks @baiwei0427 for reporting the two-prefix failure and its manual repair.

src/cli/update-cli/update-command-schema.ts
@@ -129,6 +129,13 @@ export async function preflightUpdateCommandSchemas(params: {
       for (const service of admission.services.values()) {
         if (service.serviceUpdateVerdict?.kind === "unavailable") {
           preflightNotes.push(service.serviceUpdateVerdict.message);
+        } else if (
+          service.serviceUpdateVerdict?.kind === "owned" &&
+          service.serviceUpdateVerdict.requiresInstallRootRefresh
+        ) {
+          preflightNotes.push(
+            `Gateway service targets ${service.serviceUpdateVerdict.root}; ${shouldRestart ? "would reconcile it with" : "restart is disabled; run openclaw doctor --fix to reconcile it with"} the active installation ${root}.`,
+          );
         }
       }
       const target =

6digris/openclaw-settle

src/cli/update-cli/update-command-schema.ts
@@ -161,6 +161,13 @@ export async function preflightUpdateCommandSchemas(params: {
       for (const inspectedService of admission.services.values()) {
         if (inspectedService.serviceUpdateVerdict?.kind === "unavailable") {
           preflightNotes.push(inspectedService.serviceUpdateVerdict.message);
+        } else if (
+          inspectedService.serviceUpdateVerdict?.kind === "owned" &&
+          inspectedService.serviceUpdateVerdict.requiresInstallRootRefresh
+        ) {
+          preflightNotes.push(
+            `Gateway service targets ${inspectedService.serviceUpdateVerdict.root}; ${shouldRestart ? "would reconcile it with" : "restart is disabled; run openclaw doctor --fix to reconcile it with"} the active installation ${root}.`,
+          );
         }
       }
       const target =